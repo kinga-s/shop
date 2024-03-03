3175bbc72806455f21a582a789413833c31ddd3c
KSob01 <72964417+KSob01@users.noreply.github.com>
2024-03-03 12:05:16 +0100

    Create dummy page with custom css

---
+++ frontend-shop/src/App.js
@@ -0,0 +1,18 @@
+
+
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/"> React Shopping Cart</a>
+      </header>
+      <main>
+        Product list
+      </main>
+      <footer>Academic</footer>
+    </div>
+  );
+}
+
+export default App;
